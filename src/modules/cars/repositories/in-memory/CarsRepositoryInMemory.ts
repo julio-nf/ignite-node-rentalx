d9ac4b22a5e88dd9788ca199cad4e5440ea5f7d6
Julio Novais <julio.novais@outlook.com>
2021-05-27 14:02:27 -0300

    fix: added two different logics for listing cars

--- src/modules/cars/repositories/in-memory/CarsRepositoryInMemory.ts
+++ src/modules/cars/repositories/in-memory/CarsRepositoryInMemory.ts
@@ -37,17 +37,36 @@ export class CarsRepositoryInMemory implements Cars {
 
   async findAvailable(name?: string, brand?: string, categoryId?: string): Promise<Car[]> {
     return this.cars.filter((car) => {
-      if (!car.available) return null;
-
-      if (name || brand || categoryId) {
-        if (car.name === name || car.brand === brand || car.categoryId === categoryId) {
-          return car;
-        }
-
-        return null;
+      // Esse trecho retorna apenas carros disponíveis caso não seja passado parâmetros.
+      // Caso seja passado algum parâmetro ele retornará os carros correspondentes
+      // independentemente se este está disponível ou não.
+      // ------------------------------------------------------------------
+      if (
+        (name && car.name === name) ||
+        (brand && car.brand === brand) ||
+        (categoryId && car.categoryId === categoryId) ||
+        car.available
+      ) {
+        return car;
       }
 
-      return car;
+      return null;
+
+      // Esse trecho volta APENAS carros disponíveis.
+      // Além disso, caso seja passado algum parametro e não existam carros
+      // disponíveis correspondente a esses parametros não será retornado nada.
+      // ----------------------------------------------------------------------
+      // if (!car.available) return null;
+
+      // if (name || brand || categoryId) {
+      //   if (car.name === name || car.brand === brand || car.categoryId === categoryId) {
+      //     return car;
+      //   }
+
+      //   return null;
+      // }
+
+      // return car;
     });
   }
 }
